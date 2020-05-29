a7016565dbdbe5514cf2a3bc1f6a870d67e3f08a
brunoklehm <brunowuklehm@gmail.com>
2020-05-29 00:24:01 +0000

    send and receive data

--- client.c
+++ client.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h> /* strlen */
 #include <sys/socket.h>
-#include <arpa/inet.h>
-
+#include <arpa/inet.h> /* inet_addr */
+#include <unistd.h> /* close */
 
 int main(int argc, char *argv[])
 {
     int socket_desc;
     struct sockaddr_in server;
+    char *message, server_reply[2000];
 
     /* AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP */
     socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
@@ -23,12 +25,33 @@ int main(int argc, char *argv[])
     server.sin_port = htons(80);
 
     /* connect to server */
-    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) 
+    {
         printf("Erro ao conectar-se\n");
         return 1;
     }
 
     printf("Conectado.\n");
+
+    /* send data */
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if (send(socket_desc, message, strlen(message), 0) < 0)
+    {
+        printf("Erro ao enviar\n");
+        return 1;
+    }
+    printf("Dados enviados.\n");
+
+    /* receive data */
+    if (recv(socket_desc, server_reply, 2000, 0) < 0)
+    {
+        printf("Falha no recv\n");
+        return 1;
+    }
+    printf("Resposta recebida ");
+    printf("%s\n", server_reply);
+
+    close(socket_desc);
 
     return 0;
 }
