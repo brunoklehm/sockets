528104f856a80584cc26d7934c7d3d95ecf0b70a
brunoklehm <brunowuklehm@gmail.com>
2020-05-29 00:10:27 +0000

    connect to server

--- client.c
+++ client.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+
 
 int main(int argc, char *argv[])
 {
+    int socket_desc;
+    struct sockaddr_in server;
+
     /* AF_INET - IPv4, SOCK_STREAM - tcp, 0 - IP */
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0); 
 
     if (socket_desc == -1)
     {
         printf("Não foi possível criar o socket");
         return 1;
     }
+
+    /* server to connect */
+    server.sin_addr.s_addr = inet_addr("172.217.28.132");
+    server.sin_family = AF_INET; /* IPv4 */
+    server.sin_port = htons(80);
+
+    /* connect to server */
+    if (connect(socket_desc, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        printf("Erro ao conectar-se\n");
+        return 1;
+    }
+
+    printf("Conectado.\n");
 
     return 0;
 }
